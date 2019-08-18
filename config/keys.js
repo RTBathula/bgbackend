0d4d73f282e4190403d2e94b1f629d0220f42bfb
rtbathula <battu.network@gmail.com>
2019-08-18 21:02:58 +0200

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,13 @@
 module.exports = {	
 	cors : ['http://localhost:1440','http://bullgrunt.com',,'http://www.bullgrunt.com'],
 	cloudboostKeys:{
-		appId:"bull99",
-		clientKey:"af8Ow0iegVFfWt35ENVLog=="
+		appId:"bhgfgjhuguy9",
+		clientKey:"af8ghfvukyjhfvukyjg"
 	},
 	twilioKeys:{
-		sid: 'AC06cd7752bed3954fcd9bf8d2123de8bd',
-	    token: 'bae12e2d329bb3d8364aca89a483ba42',
-	    fromNumber: '+18024481549'
+		sid: 'hhgfyigutdtuyfyi',
+	    token: 'jhgyujpiouoiuou',
+	    fromNumber: '+180247547549'
 	}	
 	
 };
